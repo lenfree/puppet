248bba17b0310045525e496f89ecc1e87e156711
lenfree Y <lenfree.yeung@gmail.com>
2013-09-01 21:26:49 +1000

    pull-updates.sh added

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,12 @@ class puppet {
 	source => 'puppet:///modules/puppet/papply.sh',
 	mode   => '0755',
     }
+
+    file { '/usr/local/bin/pull-updates':
+	source => 'puppet:///modules/puppet/pull-updates.sh',
+	mode   => '0755'
+    }
+
 }
+
+
